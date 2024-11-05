afcde62b302deccfab6186986ac43208cc966799
SupremoSenhorDoConeDeTransito <bryanoliveiratkd@gmail.com>
2024-11-05 13:28:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function getProjects() {
-    const urlGitHub = "https://github.com/SupremoSenhorDoConeDeTransito"; // Inserir o nome de usuário do seu github
+    const urlGitHub = "https://api.github.com/users/SupremoSenhorDoConeDeTransito/repos"; // Inserir o nome de usuário do seu github
     const loadingElement = document.getElementById("loading");
     const listElement = document.getElementById("my-projects-list");
   
